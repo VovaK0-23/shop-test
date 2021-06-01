eb0d094c1a213af0a13e00a4d8948e930f94ce1f
VovaK0-23 <71105454+VovaK0-23@users.noreply.github.com>
2021-06-01 17:41:27 +0200

    not create new cart if user already has one

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,13 +12,19 @@ class ApplicationController < ActionController::Base
   end
 
   def signed_user_cart
-    if session[:cart_id].present?
-      @cart = Cart.update(session[:cart_id], {user_id: current_user.id})
+    if session[:cart_id].blank?
+      create_cart
       return
     end
-    #TODO fix new cart creates even if user already have cart
-    @cart = Cart.find_or_create_by(user_id: current_user.id)
-    session[:cart_id] = @cart.id
+    if find_cart_by_user_id.blank?
+      @cart = Cart.update(session[:cart_id], { user_id: current_user.id })
+      return
+    end
+    if find_cart_by_session.cart_items.blank?
+      destroy_empty_cart
+      return
+    end
+    update_cart
   end
 
   def unsigned_user_cart
@@ -28,5 +34,38 @@ class ApplicationController < ActionController::Base
     end
     @cart = Cart.create
     session[:cart_id] = @cart.id
+  end
+
+  def create_cart
+    @cart = if find_cart_by_user_id.present?
+              find_cart_by_user_id
+            else
+              Cart.create(user_id: current_user.id)
+            end
+  end
+
+  def destroy_empty_cart
+    @cart = find_cart_by_user_id
+    return if find_cart_by_session.id == find_cart_by_user_id.id
+
+    find_cart_by_session.destroy
+    session[:cart_id] = @cart.id
+  end
+
+  def update_cart
+    if find_cart_by_session.id != find_cart_by_user_id.id
+      find_cart_by_user_id.destroy
+      @cart = Cart.update(session[:cart_id], { user_id: current_user.id })
+    else
+      @cart = find_cart_by_user_id
+    end
+  end
+
+  def find_cart_by_session
+    Cart.find(session[:cart_id])
+  end
+
+  def find_cart_by_user_id
+    Cart.find_by(user_id: current_user.id)
   end
 end
