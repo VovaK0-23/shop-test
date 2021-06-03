dda6911626e2f47259f5dde025557ad209b90b0f
VovaK0-23 <71105454+VovaK0-23@users.noreply.github.com>
2021-06-03 22:00:13 +0200

    stripe payment

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -1,5 +1,29 @@
 class PaymentController < ApplicationController
-  def new; end
+  skip_before_action :verify_authenticity_token, only: :new
 
-  def create; end
+  def new
+    order = Order.find(params[:id])
+    line_items = []
+    order.cart_items.each do |cart_item|
+      hh = {}
+      product = cart_item.product
+      hh[:price_data] = {}
+      hh[:price_data][:currency] = 'usd'
+      hh[:price_data][:unit_amount] = product.price.to_i * 100
+      hh[:price_data][:product_data] = {}
+      hh[:price_data][:product_data][:name] = product.name
+      hh[:quantity] = cart_item.amount
+      line_items << hh
+    end
+
+    session = Stripe::Checkout::Session.create({
+                                         payment_method_types: ['card'],
+                                         line_items: line_items,
+                                         mode: 'payment',
+                                         success_url: 'https://www.google.com',
+                                         cancel_url: 'https://www.google.com',
+                                     })
+
+    render json: session, only: [:id]
+  end
 end
