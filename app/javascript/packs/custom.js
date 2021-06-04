5eae9b905427a3c3e9aceb4b60be19926e292b4d
VovaK0-23 <71105454+VovaK0-23@users.noreply.github.com>
2021-06-04 21:07:03 +0200

    jquery method to add cart items
    rebase

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,8 +1,19 @@
-// $(document).ready(function(){
-//     $(".add-to-cart").click(function(){
-//         let amount_field = $(this).closest("tr").find(".amount");
-//         let amount = amount_field.text();
-//         amount = parseInt(amount) + 1;
-//         amount_field.text(amount);
-//     });
-// });
+$(document).ready(function(){
+    $(".add-to-cart").click(function(event){
+        event.preventDefault();
+        let amount_field = $(this).closest("tr").find(".amount");
+        let amount = amount_field.text();
+        amount = parseInt(amount) + 1;
+        amount_field.text(amount);
+        console.log(this.dataset.id);
+        let url = "cart_item/"+ this.dataset.id;
+        $.ajax({
+            url: url,
+            type: "get",
+            success: function() {
+                console.log('success')
+            }
+        });
+        return false
+    });
+});
