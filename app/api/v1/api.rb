c1d146b91b8907ae4937ed6e9dea7ab197640ab7
VovaK0-23 <71105454+VovaK0-23@users.noreply.github.com>
2021-06-10 17:31:33 +0200

    change routes to api

--- app/api/v1/api.rb
+++ app/api/v1/api.rb
@@ -8,7 +8,7 @@ module V1
     add_swagger_documentation(
       api_version: 'v1',
       hide_documentation_path: true,
-      mount_path: '/swagger_doc',
+      mount_path: '/api/v1/swagger_doc',
       hide_format: true
     )
   end
--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,6 +1,6 @@
 GrapeSwaggerRails.options.before_action do
-  GrapeSwaggerRails.options.app_url = request.protocol + request.host_with_port + request.path.delete_suffix('/docs/')
+  GrapeSwaggerRails.options.app_url = request.protocol + request.host_with_port
 end
 GrapeSwaggerRails.options.validator_url = nil
-GrapeSwaggerRails.options.url = '/swagger_doc'
+GrapeSwaggerRails.options.url = '/api/v1/swagger_doc'
 
