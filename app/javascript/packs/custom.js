bd33f68fdef79dd71008d948427c9a15ff7bac7b
VovaK0-23 <71105454+VovaK0-23@users.noreply.github.com>
2021-06-18 20:12:08 +0200

    modal animation

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -22,4 +22,30 @@ $(document).ready(function(){
         });
         return false
     });
+
+    let cart = $(".cart-modal");
+    $("#btn-cart").click(function(event) {
+        event.preventDefault();
+        if (cart.hasClass("hide")){
+            cart.css("right", "-19rem").addClass("animation").removeClass("hide").css("right", "0");
+            setTimeout(function(){
+                cart.removeClass("animation")
+            }, 1500);
+        } else {
+            cart.addClass("reverse-animation");
+            setTimeout(function () {
+                cart.addClass("hide").removeClass("reverse-animation")
+            }, 1500);
+        }
+    });
+
+    $('body').click(function(event){
+        if(event.target.class === "cart-modal")
+            return;
+        if(event.target.id === "btn-cart")
+            return;
+        if($(event.target).closest('.cart-modal').length)
+            return;
+        $(".cart-modal").addClass("hide");
+    });
 });
