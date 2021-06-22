024de72053c873d621db0a8f55f87cf980a313da
VovaK0-23 <71105454+VovaK0-23@users.noreply.github.com>
2021-06-22 13:41:07 +0200

    code style fix
    code style fix

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -2,57 +2,49 @@ const token = document.getElementsByName(
     "csrf-token"
 )[0].content;
 
-$(document).ready(function(){
-    $(".add-to-cart").click(function(event){
-        event.preventDefault();
-        let amount_field = $(this).closest("tr").find(".amount");
-        let amount = amount_field.text();
-        amount = parseInt(amount) + 1;
-        amount_field.text(amount);
-        let url = "cart_item/"+ this.dataset.id;
-        $.ajax({
-            url: url,
-            type: 'POST',
-            headers: {
-                'X-CSRF-Token': token
-            },
-            success: function() {
-                console.log('success')
-            }
-        });
-        return false
+$(document).ready(() => {
+  $(".add-to-cart").click(function(event) {
+    event.preventDefault();
+    let amount_field = $(this).closest("tr").find(".amount");
+    let amount = amount_field.text();
+    amount = parseInt(amount) + 1;
+    amount_field.text(amount);
+    let url = "cart_item/" + this.dataset.id;
+    $.ajax({
+      url: url,
+      type: 'POST',
+      headers: {
+        'X-CSRF-Token': token
+      },
+      success: () => console.log('success')
     });
+    return false
+  });
 
-    let cart = $(".cart-modal");
-    $("#btn-cart").click(function(event) {
-        event.preventDefault();
-        if (cart.hasClass("animation") || cart.hasClass("reverse-animation"))
-            return;
-        if (cart.hasClass("hide")){
-            cart.addClass("animation").removeClass("hide");
-            setTimeout(function(){
-                cart.removeClass("animation")
-            }, 1500);
-        } else {
-            cart.addClass("reverse-animation");
-            setTimeout(function () {
-                cart.addClass("hide").removeClass("reverse-animation")
-            }, 1000);
-        }
-    });
+  let cart = $(".cart-modal");
+  $("#btn-cart").click(event => {
+    event.preventDefault();
+    if (cart.hasClass("animation") || cart.hasClass("reverse-animation"))
+      return;
+    if (cart.hasClass("hide")) {
+      cart.addClass("animation").removeClass("hide");
+      setTimeout(() => cart.removeClass("animation"), 1500);
+    } else {
+      cart.addClass("reverse-animation");
+      setTimeout(() => cart.addClass("hide").removeClass("reverse-animation"), 1000);
+    }
+  });
 
-    $('body').click(function(event){
-        if(event.target.class === "cart-modal")
-            return;
-        if(event.target.id === "btn-cart")
-            return;
-        if($(event.target).closest('.cart-modal').length)
-            return;
-        if (cart.hasClass("animation") || cart.hasClass("reverse-animation"))
-            return;
-        cart.addClass("reverse-animation");
-        setTimeout(function () {
-            cart.addClass("hide").removeClass("reverse-animation")
-        }, 1000);
-    });
+  $('body').click(event => {
+    if (event.target.class === "cart-modal" ||
+        event.target.id === "btn-cart" ||
+        $(event.target).closest('.cart-modal').length ||
+        cart.hasClass("animation") ||
+        cart.hasClass("reverse-animation") ||
+        cart.hasClass("hide")) {
+      return;
+    }
+    cart.addClass("reverse-animation");
+    setTimeout(() => cart.addClass("hide").removeClass("reverse-animation"), 1000);
+  });
 });
--- app/javascript/packs/stripe.js
+++ app/javascript/packs/stripe.js
@@ -3,28 +3,30 @@ const orderId = $('meta[name="order-id"]').attr('content');
 const stripe = Stripe(publicKey);
 const checkoutButton = document.getElementById('checkout-button');
 const token = document.getElementsByName(
-    "csrf-token"
+  "csrf-token"
 )[0].content;
 
 checkoutButton.addEventListener('click', function() {
-    fetch('/payment/'+ orderId, {
-        method: 'POST',
-        headers: {
-            'X-CSRF-Token': token
-        }
+  fetch('/payment/' + orderId, {
+      method: 'POST',
+      headers: {
+        'X-CSRF-Token': token
+      }
     })
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(session) {
-            return stripe.redirectToCheckout({ sessionId: session.id });
-        })
-        .then(function(result) {
-            if (result.error) {
-                alert(result.error.message);
-            }
-        })
-        .catch(function(error) {
-            console.error('Error:', error);
-        });
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(session) {
+      return stripe.redirectToCheckout({
+        sessionId: session.id
+      });
+    })
+    .then(function(result) {
+      if (result.error) {
+        alert(result.error.message);
+      }
+    })
+    .catch(function(error) {
+      console.error('Error:', error);
+    });
 });
